cf4eb66b2585d0a33424341864215a2307d087a9
Dax2976 <98029197+Dax2976@users.noreply.github.com>
2022-05-09 11:56:35 +0200

    initial commit + header

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="d-flex justify-content-between bg-dark p-3">
+      <h1 class="text-uppercase text-danger">Boolflix</h1>
+      <div class="text-align-center align-items-end d-flex">
+        <input type="text" class="input-group-text" placeholder="Search a FIlm/TV Series">
+        <button class="btn bg-light btnSearch">Search</button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import'@/assets/scss/style.scss';
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.btnSearch{
+  margin-left: 10px ;
+}
+</style>
